23cdc4611a3c774094cd05cb56c6d500979b4e86
codigoescuela40 <codigoescuela4.0@educarex.es>
2026-07-21 13:19:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -48,21 +48,7 @@ window.addEventListener("DOMContentLoaded", () => {
             // Añadimos un escuchador individual por si queremos diagnosticar alguno
 
             visor.addEventListener("model-loaded", (e) => {
-            
-                e.detail.model.traverse((obj) => {
-            
-                    if (!obj.isMesh) return;
-            
-                    obj.material = new THREE.MeshStandardMaterial({
-                        color: 0xff0000,
-                        metalness: 0,
-                        roughness: 1
-                    });
-            
-                });
-            
-                console.log("Material sustituido");
-            
+                console.log("Modelo cargado");
             });
 
             
